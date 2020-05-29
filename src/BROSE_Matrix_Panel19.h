80edffdf9e464a762abf60af3d9550942a20e4d7
jp112sdl <jp112sdl@icloud.com>
2020-05-29 23:15:29 +0200

    add BROSE Panel

---
+++ src/BROSE_Matrix_Panel19.h
@@ -0,0 +1,20 @@
+#ifndef __LAWO_MATRIX_PANEL_H__
+#define __LAWO_MATRIX_PANEL_H__
+#include <Arduino.h>
+
+#define MATRIX_WIDTH 28
+#define MATRIX_HEIGHT 19
+#define PANEL_WIDTH 28
+#define COL_SWAP
+#define USE_BROSE
+
+byte PANEL_LINES[1] = {0};
+
+bool hasHalfPanelOffset(__attribute__((unused)) byte colIndex) {
+  /*
+   * Determine if the given column index belongs to a panel which was preceded by a 14-col half panel.
+   */
+
+  return false;
+}
+#endif
